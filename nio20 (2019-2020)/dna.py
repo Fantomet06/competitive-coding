2b14b43acf7f8335edd2a1d44f83d8d046d61caa
Fantomet <Sigurd.Evensen@hotmail.com>
2022-01-05 19:43:31 +0100

    Update dna.py

--- nio20 (2019-2020)/dna.py
+++ nio20 (2019-2020)/dna.py
@@ -6,22 +6,22 @@ def main():
 
     l1 = list(word1)
     l2 = []
+    b = 0
+
     for y in range(N):
         for i in range(N):
             try:
                 check = [l1[x+y] for x in range(i+1)]
             except:
                 pass
-            check2 = "".join(str(item) for item in check)
+            check2 = "".join(check)
             if check2 not in word2:
                 l2.append(check2)
-            
-
+                b += 1
     
-    if len(l2) == 0:
+    if b == 0:
         print("ingen")
     else:
-        a = min(l2, key=len)
-        print(a)
+        print(min(l2, key=len))
 main()
 
