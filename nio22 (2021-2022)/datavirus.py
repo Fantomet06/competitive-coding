5a9acbde3d744e53084f23dd475e4b032e993f86
Fantomet <Sigurd.Evensen@hotmail.com>
2022-01-14 19:29:08 +0100

    Update datavirus.py

--- nio22 (2021-2022)/datavirus.py
+++ nio22 (2021-2022)/datavirus.py
@@ -9,39 +9,84 @@ def del_vertex(k, v):
 
             return k
 
+N = 100000
+gr1 = {}
+gr2 = {}
+vis1 = [0] * N
+vis2 = [0] * N
+
+
+
+def dfs1(x) :
+    vis1[x] = True
+    if x not in gr1:
+        gr1[x] = {}
+            
+    for i in gr1[x]:
+        if (not vis1[i]):
+            dfs1(i)
+
+# DFS function
+def dfs2(x) :
+    vis2[x] = True
+
+    if x not in gr2:
+        gr2[x] = {}
+            
+    for i in gr2[x]:
+        if (not vis2[i]):
+            dfs2(i)
+
+def Is_Connected(n):
+    global vis1
+    global vis2
+        
+    vis1 = [False] * len(vis1)
+    dfs1(1)
+        
+    vis2 = [False] * len(vis2)
+    dfs2(1)
+        
+    for i in range(1, n + 1):
+        if (not vis1[i] and not vis2[i]) :
+            return False
+    return True
+
+def Add_edge(u, v) :
+ 
+    if u not in gr1 :
+        gr1[u] = []
+         
+    if v not in gr2 :
+        gr2[v] = []
+         
+    gr1[u].append(v)
+    gr2[v].append(u)
+
+def DFSUtil(graph, v, visited):
+        # Mark the current node as visited and print it
+        visited.add(v)
+
+        # recur for all the vertices adjacent to this vertex
+        for neighbour in graph[v]:
+            if neighbour not in visited:
+                DFSUtil(graph, neighbour, visited)
+        # The function to do DFS traversal. It uses recursive DFSUtil
+
+def DFS(graph):
+    # create a set to store all visited vertices
+    visited = set()
+    # call the recursive helper function to print DFS traversal starting from all
+    # vertices one by one
+    for vertex in graph:
+        if vertex not in visited:
+            DFSUtil(graph, vertex, visited)
+
 def main():
-    def BFS(adj, src, dest, v, pred, dist):
-        queue = []
-    
-        visited = [False for i in range(v)]
-    
-        for i in range(v):
-            dist[i] = 1000000
-            pred[i] = -1
-
-        visited[src] = True
-        dist[src] = 0
-        queue.append(src)
-        while (len(queue) != 0):
-            u = queue[0]
-            queue.pop(0)
-            try:
-                for i in range(len(adj[u])):
-                    if (visited[adj[u][i]] == False):
-                        visited[adj[u][i]] = True
-                        dist[adj[u][i]] = dist[u] + 1
-                        pred[adj[u][i]] = u
-                        queue.append(adj[u][i])
-                        if (adj[u][i] == dest):
-                            return True
-            except:
-                pass
-        return False
-
     adj_list = {}
     mylist = []
     
-    def add_edge(node, node1, node2):
+    """def add_edge(node, node1, node2):
         if node not in mylist:
             mylist.append(node)
             
@@ -53,10 +98,10 @@ def main():
         elif node1 in adj_list:
             temp.extend(adj_list[node1])
             temp.append(node2)
-            adj_list[node1] = temp
+            adj_list[node1] = temp"""
 
 
-    N, M = map(int, input().split())
+    V, M = map(int, input().split())
 
     from itertools import combinations
     import copy
@@ -65,8 +110,8 @@ def main():
 
     for i in range(M):
         a, b = map(int, input().split())
-        add_edge(a,a,b)
-        add_edge(b,b,a)
+        Add_edge(a,b)
+        #Add_edge(b,a)
         if a not in clist:
             clist.append(a)
         
@@ -74,7 +119,7 @@ def main():
             clist.append(b)
     aconnections = []
 
-    for x in range(N):
+    for x in range(V):
         #print(adj_list)
         adj_list2 = copy.deepcopy(adj_list)
         adj_list2  = del_vertex(adj_list2, x)
@@ -87,11 +132,12 @@ def main():
         connections = 0
 
         for i in range(len(combination2)):
-            pred=[0 for i in range(N)]
-            dist=[0 for i in range(N)]
-            bfs = BFS(adj_list2, combination2[i][0], combination2[i][1], N, pred, dist)
-            if bfs != False:
+            #pred=[0 for i in range(N)]
+            #dist=[0 for i in range(N)]
+            if (Is_Connected(V)):
                 connections += 1
+            else:
+                pass
         
         aconnections.append(connections)
 
